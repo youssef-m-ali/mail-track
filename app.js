2d96a27a1ba0c18f98618f65ca4940e3ca87c1a0
Youssef Ali <youssef.mym@yahoo.com>
2020-11-24 03:20:43 -0330

    Implement mail tracker logic

--- app.js
+++ app.js
@@ -1,11 +1,91 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const Mail = require('./models/mail');
+const Creds = require('./creds');
+const url = require('url');
+
+
 const app = express();
 
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+const dbURI = Creds.db;
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('welcome to the home');
+    
+    var total = 0;
+    Mail.countDocuments({}, (err, count) =>{
+        total = count;
+        if (err) throw err;
+    });
+
+    sleep(1000).then(()=>{
+    res.send(`Next mail is ${total}`);
+    });
+
 })
 
-const port = process.env.port || 3000;
+app.get('/image', (req, res) =>{
+    var mail_id = req.query.id;
+
+    Mail.findOne({id : mail_id}, (err, result)=>{
+
+        var date = new Date();
+        date = date.toString();
+
+        result.readat.push(date);
+        result.save();
+
+        })
+        .catch((err)=>{
+            if (err) throw err;
+        });
+
+    res.sendFile(path.join(__dirname, 'public', '1x1.png'));
+
+})
+
+
+// TO BE IMPLEMENTED
+
+app.get('/add-mail', (req, res) => {
+
+    var total = 0;
+    Mail.countDocuments({}, (err, count) =>{
+        total = count;
+        if (err) throw err;
+    });
+
+    sleep(1000).then(()=>{
+        console.log('generating new mail')
+        
+        const mail = new Mail({
+            id: total,
+            title: "new mail",
+            to: ""
+        });
+
+        mail.save()
+            .then((result)=>{
+                res.send(result)
+            })
+            .catch((err)=>{
+                if (err) throw err;
+            });
+    });
+
+});
+
+const port = process.env.port || 8080;
 
 app.listen(port, () =>{
     console.log('we started');
